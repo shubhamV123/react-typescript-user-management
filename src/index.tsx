26627eb290b24e614f527b9b8da66fd2b7012085
Shubham <sv.code97@gmail.com>
2019-09-16 23:26:22 +0530

    setup webpack and typscript and added hooks for prettify code

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import "./styles/style.css";
+import Counter from "./components/Hello";
+
+ReactDOM.render(<Counter />, document.getElementById("root"));
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  //Tell webpack this is entry file
+  entry: "./src/index.tsx",
+  //this is webpack going to bundle all assets
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.min.js"
+  },
+  resolve: {
+    //Attempt to resolve these extensions in order.
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  module: {
+    //All files with the extension .tsx or .ts should be processed by awesome-typescript-loader
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.tsx?$/,
+        loader: "awesome-typescript-loader"
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html"
+    })
+  ]
+};
